55b4ff108a49d84ba1a0693240b01aeea1ece865
matiasgluck <matiasugluck@gmail.com>
2025-03-11 20:05:37 -0300

    initial scaffolding

---
+++ src/main/java/com/matiasugluck/deremate_backend/DeremateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.matiasugluck.deremate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeremateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeremateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matiasugluck/deremate_backend/DeremateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasugluck.deremate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeremateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
