0578539bc22c14ae8b0864def68582625c17df5a
17sTomy <tomasignacioalv@hotmail.com>
2025-04-05 18:50:14 -0300

    add messages

---
+++ src/main/java/com/matiasugluck/deremate_backend/constants/AuthApiMessages.java
@@ -0,0 +1,39 @@
+package com.matiasugluck.deremate_backend.constants;
+
+public class AuthApiMessages {
+    public static final String NOT_EXISTING_USER = "No user exists with the provided email.";
+    public static final String EMAIL_NOT_VERIFIED = "The email has not been verified.";
+    public static final String INVALID_CREDENTIALS = "Invalid credentials.";
+    public static final String USER_DISABLED = "User is disabled.";
+    public static final String LOGIN_SUCCESSFUL = "Login successful.";
+    public static final String ALREADY_EXISTING_EMAIL = "Email already exists.";
+    public static final String ERROR_SENDING_VERIFICATION_EMAIL = "Error sending verification email: ";
+    public static final String USER_REGISTERED_SUCCESSFULLY = "User registered successfully. A verification email has been sent to ";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/matiasugluck/deremate_backend/constants/DeliveryApiMessages.java
@@ -0,0 +1,25 @@
+package com.matiasugluck.deremate_backend.constants;
+
+public class DeliveryApiMessages {
+    public static final String INVALID_PRODUCTS_CODE = "INVALID_PRODUCTS";
+    public static final String INVALID_PRODUCTS_DESC = "Lista inválida.";
+    public static final String INTERNAL_ERROR_CODE = "INTERNAL_ERROR";
+    public static final String INTERNAL_ERROR_DESC = "Error al crear el delivery.";
+    public static final String INVALID_PIN_CODE = "INVALID_PIN";
+    public static final String INVALID_PIN_DESC = "El pin incorrecto.";
+    public static final String DELIVERY_NOT_FOUND_CODE = "DELIVERY_NOT_FOUND";
+    public static final String DELIVERY_NOT_FOUND_DESC = "Delivery not found.";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
