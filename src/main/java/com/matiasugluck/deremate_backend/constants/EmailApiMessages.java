a1de937fa86ea7f60c824f4c506dfeb80abcdf81
17sTomy <tomasignacioalv@hotmail.com>
2025-04-05 18:50:14 -0300

    add messages

---
+++ src/main/java/com/matiasugluck/deremate_backend/constants/EmailApiMessages.java
@@ -0,0 +1,8 @@
+package com.matiasugluck.deremate_backend.constants;
+
+public class EmailApiMessages {
+    public static final String EMAIL_VERIFICATION = "Email Verification";
+    public static final String YOUR_VERIFICATION_CODE_IS = "Your verification code is: ";
+    public static final String PASSWORD_RESET = "Password Reset";
+    public static final String YOUR_PASSWORD_RESET_TOKEN_IS = "Your password reset token is: ";
+}
