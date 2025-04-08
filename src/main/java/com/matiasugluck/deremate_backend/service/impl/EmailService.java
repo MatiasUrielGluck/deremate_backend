babc8ad43195c0dfcc326d7eb3574ebe44316624
matiasgluck <matiasugluck@gmail.com>
2025-04-08 00:13:14 -0300

    feat: async configuration and async email sending

--- src/main/java/com/matiasugluck/deremate_backend/service/impl/EmailService.java
+++ src/main/java/com/matiasugluck/deremate_backend/service/impl/EmailService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
@@ -40,6 +41,7 @@ public class EmailService {
         });
     }
 
+    @Async
     public void sendVerificationEmail(String to, String code) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
