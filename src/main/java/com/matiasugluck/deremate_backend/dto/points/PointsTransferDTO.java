ea966a7704a481713796153558dc1d9ef3552c9e
17sTomy <tomasignacioalv@hotmail.com>
2025-04-09 23:27:24 -0300

    add points service interface

---
+++ src/main/java/com/matiasugluck/deremate_backend/dto/points/PointsTransferDTO.java
@@ -0,0 +1,14 @@
+package com.matiasugluck.deremate_backend.dto.points;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PointsTransferDTO {
+    private Long fromUserId;
+    private Long toUserId;
+    private int amount;
+}
