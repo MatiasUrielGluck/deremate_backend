f6290a090c59efbc5a95a09b9834bfb175177051
lucasRubiniF1 <lrubini@uade.edu.ar>
2025-04-12 22:16:30 -0300

    Update GenericResponseDTO with statusCode and constructors

--- src/main/java/com/matiasugluck/deremate_backend/dto/auth/LoginRequestDTO.java
+++ src/main/java/com/matiasugluck/deremate_backend/dto/auth/LoginRequestDTO.java
@@ -1,17 +1,18 @@
 package com.matiasugluck.deremate_backend.dto.auth;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class LoginRequestDTO {
-    @NotNull
-    @Email
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "El correo no tiene un formato válido")
     private String email;
 
-    @NotNull
+    @NotBlank(message = "La contraseña es obligatoria")
     private String password;
 }
