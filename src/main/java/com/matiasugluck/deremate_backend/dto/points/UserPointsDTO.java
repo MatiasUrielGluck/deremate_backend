aae1ba2179a28d2ba18902fb1edf544bed1c87ea
17sTomy <tomasignacioalv@hotmail.com>
2025-04-17 14:05:04 -0300

    add points into db model

---
+++ src/main/java/com/matiasugluck/deremate_backend/dto/points/UserPointsDTO.java
@@ -0,0 +1,17 @@
+package com.matiasugluck.deremate_backend.dto.points;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserPointsDTO {
+    private Long userId;
+    private int points;
+    private int level;
+    private int pointsToNextLevel;
+}
