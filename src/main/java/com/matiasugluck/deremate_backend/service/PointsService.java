0e82d52bbeeaf67e18d457a0c61ad18ac8818b25
17sTomy <tomasignacioalv@hotmail.com>
2025-04-17 14:35:36 -0300

    add messages file

--- src/main/java/com/matiasugluck/deremate_backend/service/PointsService.java
+++ src/main/java/com/matiasugluck/deremate_backend/service/PointsService.java
@@ -4,6 +4,7 @@ import com.matiasugluck.deremate_backend.dto.points.UserPointsDTO;
 
 public interface PointsService {
     void addPointsForCompletedDelivery(Long userId);
+    void addPointsByDistance(Long userId, double kilometers);
     UserPointsDTO getUserPointsInfo(Long userId);
     double getProgressPercentage(Long userId);
     void subtractPoints(Long userId, int points);
