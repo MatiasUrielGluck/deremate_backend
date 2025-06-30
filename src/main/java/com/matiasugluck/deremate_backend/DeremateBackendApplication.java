aaaa944b910f8043f27fd0e21c29941ccc944559
Sebexe <sebyex304@gmail.com>
2025-06-30 19:48:49 -0300

    Agregado el envio y registro de un token de firebase en la base de datos

--- src/main/java/com/matiasugluck/deremate_backend/DeremateBackendApplication.java
+++ src/main/java/com/matiasugluck/deremate_backend/DeremateBackendApplication.java
@@ -1,10 +1,26 @@
 package com.matiasugluck.deremate_backend;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DeremateBackendApplication {
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder().setCredentials(googleCredentials).build();
+        FirebaseApp firebaseApp =  FirebaseApp.initializeApp(firebaseOptions, "deremate-app");
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeremateBackendApplication.class, args);
