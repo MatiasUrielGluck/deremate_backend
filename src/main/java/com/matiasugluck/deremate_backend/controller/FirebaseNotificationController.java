6f6192bb520c4b52e6e248cd4e3cd29a6bbd321d
Sebexe <sebyex304@gmail.com>
2025-07-01 19:48:03 -0300

    Arreglados los tokens

--- src/main/java/com/matiasugluck/deremate_backend/controller/FirebaseNotificationController.java
+++ src/main/java/com/matiasugluck/deremate_backend/controller/FirebaseNotificationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/push")
+@RequestMapping(value = "${base-path-v1}/notification")
 public class FirebaseNotificationController {
 
     private final FirebaseNotificationService firebaseNotificationService;
