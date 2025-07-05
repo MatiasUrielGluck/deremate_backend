0a68f6066d73685d796d563c8ea75d6d5b0a3b2b
Sebexe <sebyex304@gmail.com>
2025-07-05 18:51:16 -0300

    Cambio en el retorno de la lista de productos

---
+++ src/main/java/com/matiasugluck/deremate_backend/dto/product/ProductListResponseDTO.java
@@ -0,0 +1,8 @@
+package com.matiasugluck.deremate_backend.dto.product;
+
+import com.matiasugluck.deremate_backend.dto.GenericResponseDTO;
+
+import java.util.List;
+
+public class ProductListResponseDTO extends GenericResponseDTO<List<ProductDTO>> {
+}
